843de8cf90d4c2d37ac1d0eb2d8f889360a1ae88
Mauro-Bolado <maurovizoso@gmail.com>
2022-06-11 13:11:18 -0500

    Nuevo proyecto

---
+++ pcc/api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+import models
+
+def login(request):
+    return HttpResponse('NOT IMPLEMENTED')
+
+def militants(request):
+    
